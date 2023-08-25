258e7aea0c35361e3050f876365c87a5ffb34435
chiefJurist <anthonynnanna004@gmail.com>
2023-08-25 21:58:28 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+    <h1>111</h1>
+	<RouterView />
+</template>
+
+<style>
+nav a.router-link-exact-active {
+  color: var(--color-text);
+}
+
+nav a.router-link-exact-active:hover {
+  background-color: transparent;
+}
+
+nav a {
+  display: inline-block;
+  padding: 0 1rem;
+  border-left: 1px solid var(--color-border);
+}
+
+nav a:first-of-type {
+  border: 0;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<script>
+</script>
+
+<template>
+    <main></main>
+</template>
