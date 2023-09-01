496e80fd1a51a65800a70de0904ffcb3670def30
chiefJurist <anthonynnanna004@gmail.com>
2023-09-01 11:08:12 +0100

    Another commit

--- src/components/ContactMap.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <iframe class="w-full" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3964.500419727242!2d3.579945775966812!3d6.458101093533452!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103bf9dfea013439%3A0x19606d9afd8ac7af!2sDarice%20and%20Oaks%20International%20School!5e0!3m2!1sen!2sng!4v1691555738697!5m2!1sen!2sng" width="600" height="550" style="border:0;" allowfullscreen="true" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-</template>
