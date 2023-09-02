8b508dc72b9a1805d75356f8245619b307dd940b
chiefJurist <anthonynnanna004@gmail.com>
2023-09-02 20:21:58 +0100

    update

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -3,18 +3,18 @@
         <form class="main-form">
             <div class="input-con">
                 <label class="label">Username / Email:</label><br>
-                <input type="text" placeholder="input username or email" class="main-input">
+                <input type="text" placeholder="input username or email" class="main-input" required>
                 <div></div>
             </div>
 
             <div class="input-con">
                 <label class="label">Password:</label>
-                <input type="password" placeholder="input password" class="main-input">
+                <input type="password" placeholder="input password" class="main-input" required>
                 <div></div>
             </div>
 
             <div class="submit-con">
-                <input type="submit" value="Log In" class="main-submit">
+                <input type="submit" value="Log In" class="main-submit" required>
             </div>
         </form>
     </div>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -3,26 +3,39 @@
         <form class="main-form">
             <div class="input-con">
                 <label class="label"> New Email:</label><br>
-                <input type="text" placeholder="input new email" class="main-input">
+                <input type="email" placeholder="input new email" class="main-input" required>
                 <div></div>
             </div>
 
             <div class="input-con">
                 <label class="label">New Username:</label><br>
-                <input type="text" placeholder="input new username" class="main-input">
+                <input type="text" placeholder="input new username" class="main-input" required>
                 <div></div>
             </div>
 
             <div class="input-con">
                 <label class="label">New Password:</label>
-                <input type="password" placeholder="input new password" class="main-input">
+                <input type="password" placeholder="input new password" class="main-input" required>
                 <div></div>
             </div>
 
             <div class="input-con">
                 <label class="label">Confirm Password:</label>
-                <input type="password" placeholder="confirm new password" class="main-input">
+                <input type="password" placeholder="confirm new password" class="main-input" required>
                 <div></div>
+            </div>
+
+            <div class="input-con">
+                <label class="label">Usage: </label>
+                <select class="sm:text-xl font-bold p-1 sm:p-3 rounded-lg w-4/5" required>
+                    <option value="Consumer">Consumer</option>
+                    <option value="Merchant">Merchant</option>
+                </select>
+            </div>
+
+            <div class=" mt-10">
+                <input type="checkbox" required>
+                <label>Accept terms and conditions</label>
             </div>
 
             <div class="submit-con">
