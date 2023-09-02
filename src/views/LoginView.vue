84a73d6ac840deb460277e55e32b272efbbddbd3
chiefJurist <anthonynnanna004@gmail.com>
2023-09-02 18:58:43 +0100

    update

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,3 +1,21 @@
 <template>
-    <div>Log In</div>
+    <div class="form-con">
+        <form class="main-form">
+            <div class="input-con">
+                <label class="label">Username / Email:</label><br>
+                <input type="text" placeholder="input username or email" class="main-input">
+                <div></div>
+            </div>
+
+            <div class="input-con">
+                <label class="label">Password:</label>
+                <input type="password" placeholder="input password" class="main-input">
+                <div></div>
+            </div>
+
+            <div class="submit-con">
+                <input type="submit" value="Log In" class="main-submit">
+            </div>
+        </form>
+    </div>
 </template>
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,3 +1,33 @@
 <template>
-    <div>Register</div>
+    <div class="form-con">
+        <form class="main-form">
+            <div class="input-con">
+                <label class="label"> New Email:</label><br>
+                <input type="text" placeholder="input new email" class="main-input">
+                <div></div>
+            </div>
+
+            <div class="input-con">
+                <label class="label">New Username:</label><br>
+                <input type="text" placeholder="input new username" class="main-input">
+                <div></div>
+            </div>
+
+            <div class="input-con">
+                <label class="label">New Password:</label>
+                <input type="password" placeholder="input new password" class="main-input">
+                <div></div>
+            </div>
+
+            <div class="input-con">
+                <label class="label">Confirm Password:</label>
+                <input type="password" placeholder="confirm new password" class="main-input">
+                <div></div>
+            </div>
+
+            <div class="submit-con">
+                <input type="submit" value="Register" class="main-submit">
+            </div>
+        </form>
+    </div>
 </template>
